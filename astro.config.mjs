637109c4cf6599d8d1091cdcef68706c48d3f529
soush <soush915@gmail.com>
2025-04-28 22:42:37 -0700

    add checkout pages and home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+
+import node from '@astrojs/node';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  site: 'https://www.sambalogie.com',
+  integrations: [sitemap()]
+});
